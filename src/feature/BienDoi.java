8c6eca0a0d6e91cf0547dc293edde83de96656a6
duongnt204538 <duong.nt204538@sis.hust.edu.vn>
2022-12-30 19:27:51 +0700

    update menubar 2.0

--- src/feature/BienDoi.java
+++ src/feature/BienDoi.java
@@ -2,6 +2,12 @@ package feature;
 
 import javax.swing.*;
 
-public class ThayDoi extends JPanel {
-
+public class BienDoi extends JPanel {
+    enum Type {Them, ThayDoi, Chuyen};
+    
+    public BienDoi(Type type){
+        if(type == Type.Them) {
+            
+        } 
+    }
 }
